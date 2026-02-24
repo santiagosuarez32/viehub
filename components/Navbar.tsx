c6428b416cc92c03fc9ec8d98bf8b92047a9c63d
santiagosuarez32 <dlsantii86@gmail.com>
2026-02-24 20:47:43 -0300

    sdfsdf

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -95,7 +95,7 @@ export default function Navbar({ locale }: { locale: string }) {
       className={navClasses}
     >
 
-      <div className="max-w-7xl mx-auto flex justify-between items-center px-6 py-2 md:py-3">
+      <div className="max-w-7xl mx-auto flex justify-between items-center px-6 py-4 md:py-5">
 
         <Link 
           href={"/" + locale} 
@@ -107,9 +107,9 @@ export default function Navbar({ locale }: { locale: string }) {
           }}
         >
           <img 
-            src="/logo.png" 
+            src="/logo.webp" 
             alt="VieHub Logo" 
-            className="h-20 w-20 md:h-32 md:w-32 object-contain"
+            className="h-24 w-24 md:h-32 md:w-32 object-contain"
           />
         
         </Link>
